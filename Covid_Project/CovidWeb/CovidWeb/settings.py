896587a80230051b1abe817aff6eb3fb4e5e468e
nahyeon cho <whskgus12@nate.com>
2022-10-26 01:08:24 +0900

    #13 - heroku 연동 설정
    settings.py에 heroku 설정 작성

--- Covid_Project/CovidWeb/CovidWeb/settings.py
+++ Covid_Project/CovidWeb/CovidWeb/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 import dj_database_url
+import django_heroku
 import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())

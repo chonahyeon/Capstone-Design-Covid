0661c6e9bdf73dd407c0bff366f082d8d013b442
nahyeon cho <whskgus12@nate.com>
2022-10-26 19:17:52 +0900

    #13 - 헤로쿠 오류 해결
    헤로쿠 배포시에 보안상의 이유로 분리해두었던 mysetting.py가 오류였던 것 같습니다. 따라서 .env 파일로 환경변수 설정해두고 heroku에도 config vars를 이용하여 환경변수 추가 완료했습니다.

--- CovidWeb/settings.py
+++ CovidWeb/settings.py
@@ -10,21 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
+import os,environ
 from pathlib import Path
-import my_settings
+
 import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.path.join(BASE_DIR,'secrets.json')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
@@ -80,7 +86,17 @@ WSGI_APPLICATION = 'CovidWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = my_settings.DATABASES
+DATABASES = {
+'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': '3306'
+}}
+
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 

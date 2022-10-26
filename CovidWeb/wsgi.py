497a80acb533f5e51cc4c8499b66190d1b59b223
nahyeon cho <whskgus12@nate.com>
2022-10-26 19:58:00 +0900

    #13 - heroku 연동 오류 해결 완료
    정적 파일 할당 문제인가 싶어, whitenoise command를 wsgi 파일에 추가합니다.

--- CovidWeb/wsgi.py
+++ CovidWeb/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CovidWeb.settings')
 application = get_wsgi_application()
+application = WhiteNoise(application)
